a872064c259186e031fe960124bae9dd2ed60314
gwizzle651 <gwizzle651@proton.me>
2026-03-26 15:18:07 -0500

    seth please fix

--- dataRead.py
+++ dataRead.py
@@ -6,13 +6,25 @@ reader = MFRC522(spi_id=0,sck=6,miso=4,mosi=7,cs=5,rst=22)
 print("Bring TAG closer...")
 print("")
 
+detecting = False
+
 while True:
     reader.init()
     (stat, tag_type) = reader.request(reader.REQIDL)
-    if stat == reader.OK:
-        (stat, uid) = reader.SelectTagSN()
+    if detecting == False:
         if stat == reader.OK:
-            card = int.from_bytes(bytes(uid),"little",False)
-            print("CARD ID: "+str(card))
-utime.sleep_ms(500)
+            detecting= True
+            (stat, uid) = reader.SelectTagSN()
+            if stat == reader.OK:
+                card = int.from_bytes(bytes(uid),"little",False)
+                print("CARD ID: "+str(card))
+
+    elif detecting == True:
+        if stat == reader.NOTAGERR:
+            detecting = False
+        else::
+                card = int.from_bytes(bytes(uid),"little",False)
+                print("CARD ID: "+str(card))
+
+    utime.sleep_ms(50)
 
