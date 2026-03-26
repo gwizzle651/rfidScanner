82cf32bd83f5aa68f7f9cb001124d98e9e9a305a
garply <garply65@proton.me>
2026-03-26 15:44:17 -0500

    Add edge detect to dataRead.py

--- dataRead.py
+++ dataRead.py
@@ -3,7 +3,7 @@ import utime
 
 reader = MFRC522(spi_id=0,sck=6,miso=4,mosi=7,cs=5,rst=22)
 
-print("Bring TAG closer...")
+print("Awaiting Tag")
 print("")
 
 detecting = False
@@ -11,20 +11,15 @@ detecting = False
 while True:
     reader.init()
     (stat, tag_type) = reader.request(reader.REQIDL)
-    if detecting == False:
-        if stat == reader.OK:
-            detecting= True
-            (stat, uid) = reader.SelectTagSN()
-            if stat == reader.OK:
-                card = int.from_bytes(bytes(uid),"little",False)
-                print("CARD ID: "+str(card))
-
-    elif detecting == True:
-        if stat == reader.NOTAGERR:
-            detecting = False
-        else::
-                card = int.from_bytes(bytes(uid),"little",False)
-                print("CARD ID: "+str(card))
-
+    # If we were not previously detecting and we are now detecting then print data and mark previous detection
+    if detecting == False && stat == reader.OK:
+        detecting = True
+        (stat, uid) = reader.SelectTagSN()
+        card = int.from_bytes(bytes(uid),"little",False)
+        print("CARD ID: " + str(card))
+    # If we were detecting and no longer detecting then mark as no longer detecting
+    elif detecting == True && stat == reader.NOTAGERR:
+        detecting = False
+        
     utime.sleep_ms(50)
 
