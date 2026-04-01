57130dcc3cee8faadc6d05b7493f7f5c1de6160d
garply <garply65@proton.me>
2026-04-01 15:09:37 -0500

    fix bugs in dataRead.py

--- dataRead.py
+++ dataRead.py
@@ -1,5 +1,5 @@
 from mfrc522 import MFRC522
-import utime
+from time import sleep
 
 reader = MFRC522(spi_id=0,sck=6,miso=4,mosi=7,cs=5,rst=22)
 
@@ -12,14 +12,12 @@ while True:
     reader.init()
     (stat, tag_type) = reader.request(reader.REQIDL)
     # If we were not previously detecting and we are now detecting then print data and mark previous detection
-    if detecting == False && stat == reader.OK:
+    if (detecting == False and stat == reader.OK):
         detecting = True
         (stat, uid) = reader.SelectTagSN()
         card = int.from_bytes(bytes(uid),"little",False)
         print("CARD ID: " + str(card))
     # If we were detecting and no longer detecting then mark as no longer detecting
-    elif detecting == True && stat == reader.NOTAGERR:
+    elif (detecting == True and stat == reader.ERR):
         detecting = False
-        
-    utime.sleep_ms(50)
 
