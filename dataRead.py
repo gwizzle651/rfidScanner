4a8bd53c25fba6766dd160a02b8c7981d8364173
gwizzle651 <gwizzle651@proton.me>
2026-04-02 14:44:45 -0500

    add local logging

--- dataRead.py
+++ dataRead.py
@@ -3,9 +3,6 @@ from time import sleep
 
 reader = MFRC522(spi_id=0,sck=6,miso=4,mosi=7,cs=5,rst=22)
 
-print("Awaiting Tag")
-print("")
-
 detecting = False
 
 while True:
@@ -17,7 +14,13 @@ while True:
         (stat, uid) = reader.SelectTagSN()
         card = int.from_bytes(bytes(uid),"little",False)
         print("CARD ID: " + str(card))
+
+        with open("log.txt", "a") as file:
+            file.write(f"Card ID: {str(card)}\n")
+
     # If we were detecting and no longer detecting then mark as no longer detecting
     elif (detecting == True and stat == reader.ERR):
         detecting = False
+
+    sleep(.05)
 
